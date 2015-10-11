eb03f3001014837acb3dd5007793765c61fda87f
Jeremy Allison <jeremy.allison@clarecontrols.com>
2015-10-11 17:15:08 -0400

    Chapter 1&2

---
+++ Chapter2 Variables & Types/src/Exercise2_2Date.java
@@ -0,0 +1,16 @@
+//Instructions
+//Create a new program named Date.java. Copy or type in something like the “Hello, World” program and make sure 
+//you can compile and run it.
+//Following the example in Section 2.4, write a program that creates variables named day, date, month and year. 
+//day will contain the day of the week and date will contain the day of the month. What type is each variable? 
+//Assign values to those variables that represent today’s date.
+
+public class Exercise2_2Date {
+
+	public static void main(String[] args) {
+		System.out.println("Hello There!!!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
